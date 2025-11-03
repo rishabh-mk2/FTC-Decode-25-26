c32f23d87d80cb42cc474553d0ad69f6d10ec186
shreyas-mk2 <shreyas@mk2robotics.net>
2025-11-03 10:10:55 -0800

    simple teleop for Pinpoint test
    also starting writing the base Intake.java file which will be used in auto/teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpmodes/testTeleOp.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.testOpmodes;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Configurable
+@TeleOp
+public class testTeleOp extends OpMode {
+    private Follower follower;
+    public static Pose startingPose;
+    private boolean automatedDrive;
+    private TelemetryManager telemetryM;
+
+    @Override
+    public void init() {
+        automatedDrive = false;
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        telemetryM.update();
+
+        if (!automatedDrive) {
+            follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x,
+                    true
+            );
+        }
+        telemetryM.debug("position", follower.getPose());
+        telemetryM.debug("velocity", follower.getVelocity());
+    }
+}
