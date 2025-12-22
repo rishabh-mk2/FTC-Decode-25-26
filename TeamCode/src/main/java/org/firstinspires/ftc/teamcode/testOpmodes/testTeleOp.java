54062fe89b860605b6e4a6824ccbe1a236b22d9b
rishabh-mk2 <rishabh@mk2robotics.net>
2025-12-21 22:35:04 -0800

    some work on intaking/spindexing logic?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpmodes/testTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpmodes/testTeleOp.java
@@ -4,6 +4,8 @@ import com.arcrobotics.ftclib.controller.PIDController;
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -13,64 +15,63 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.List;
 
 @Configurable
 @TeleOp(name = "test teleop", group = "TeleOp")
-
 public class testTeleOp extends OpMode {
-
 
     private Follower follower;
     public static Pose startingPose;
     private boolean automatedDrive;
 
-    double p;
-    double i;
-    double d;
     PIDController pidController;
 
-    DcMotorEx intake;
-    DcMotorEx leftShooter;
-    DcMotorEx rightShooter;
-    DcMotorEx turret;
-
-    Servo spin1;
-    Servo spin2;
-    Servo kicker;
-    Servo hood;
+    DcMotorEx intake, leftShooter, rightShooter, turret;
+    Servo spin1, spin2, kicker, hood;
 
     Limelight3A limelight;
+    LLResult result;
+    double pid;
 
     RevColorSensorV3 front;
-    RevColorSensorV3 back1;
-    RevColorSensorV3 back2;
-
-    boolean intaking = false;
-    boolean spindexing = true;
-
-    boolean kickerUp1 = false;
-    boolean kickerDown1 = false;
-
-    boolean kickerUp2 = false;
-    boolean kickerDown2 = false;
-
-    boolean kickerUp3 = false;
-    boolean kickerDown3 = false;
-    boolean rotateSpin120_1 = false;
-    boolean rotateSpin120_2 = false;
-    boolean rotateSpin120_3 = false;
-
-
-    boolean rotateSpin60 = false;
-
-    boolean shoot = false;
 
     ElapsedTime runtime = new ElapsedTime();
-    double time = 0.0;
-    double frontDistance = 0.0;
+    double time;
 
+    static final double BALL_DIST_MM = 30;
+    static final double SPIN_STEP = 0.169;
+
+    int ballsLoaded = 0;
+    boolean ballDetectedLast = false;
+
+    enum IntakeState {
+        IDLE,
+        INTAKING,
+        INDEXING
+    }
+
+    IntakeState intakeState = IntakeState.IDLE;
+
+    boolean shootRequested = false;
     boolean AisPressed = false;
+
+    enum ShootState {
+        IDLE,
+        KICK_UP,
+        KICK_DOWN,
+        REINDEX
+    }
+
+    ShootState shootState = ShootState.IDLE;
+
+    void rotateSpindexer120() {
+        spin1.setPosition(spin1.getPosition() + SPIN_STEP);
+        spin2.setPosition(spin2.getPosition() + SPIN_STEP);
+    }
 
     @Override
     public void init() {
@@ -78,54 +79,29 @@ public class testTeleOp extends OpMode {
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
 
-        intake = this.hardwareMap.get(DcMotorEx.class, "intake");
-        leftShooter = this.hardwareMap.get(DcMotorEx.class, "leftShooter");
-        rightShooter = this.hardwareMap.get(DcMotorEx.class, "rightShooter");
-        turret = this.hardwareMap.get(DcMotorEx.class, "turret");
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        leftShooter = hardwareMap.get(DcMotorEx.class, "leftShooter");
+        rightShooter = hardwareMap.get(DcMotorEx.class, "rightShooter");
+        turret = hardwareMap.get(DcMotorEx.class, "turret");
 
-        spin1 = this.hardwareMap.get(Servo.class, "spin1");
-        spin2 = this.hardwareMap.get(Servo.class, "spin2");
-        kicker = this.hardwareMap.get(Servo.class, "kicker");
-        hood = this.hardwareMap.get(Servo.class, "hood");
+        spin1 = hardwareMap.get(Servo.class, "spin1");
+        spin2 = hardwareMap.get(Servo.class, "spin2");
+        kicker = hardwareMap.get(Servo.class, "kicker");
+        hood = hardwareMap.get(Servo.class, "hood");
 
-        limelight = this.hardwareMap.get(Limelight3A.class, "limelight");
+        front = hardwareMap.get(RevColorSensorV3.class, "front");
+
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
-
-//        front = this.hardwareMap.get(RevColorSensorV3.class, "front");
-//        back1 = this.hardwareMap.get(RevColorSensorV3.class, "back1");
-//        back2 = this.hardwareMap.get(RevColorSensorV3.class, "back2");
-
-        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightShooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        kicker.setPosition(0.225);
-
-        leftShooter.setDirection(DcMotorEx.Direction.FORWARD);
-        rightShooter.setDirection(DcMotorEx.Direction.REVERSE);
-        leftShooter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        rightShooter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-//        telemetry.setAutoClear(true);
-
-        p = 0.02;
-        i = 0.0;
-        d = 0.5;
-        pidController = new PIDController(p, i, d);
-        pidController.setPID(p, i, d);
-
-        spin1.setPosition(0.06 + 0.169*1.5);
-        spin2.setPosition(0.06 + 0.169*1.5);
-
         limelight.start();
 
-        follower.update();
+        kicker.setPosition(0.225);
+        spin1.setPosition(0.06);
+        spin2.setPosition(0.06);
+
+        pidController = new PIDController(0.02, 0, 0.5);
 
         runtime.reset();
-
-//        telemetry.addData("Status", "Initialized");
-//        telemetry.update();
     }
 
     @Override
@@ -135,6 +111,8 @@ public class testTeleOp extends OpMode {
 
     @Override
     public void loop() {
+
+        // DRIVE
         follower.update();
         if (!automatedDrive) {
             follower.setTeleOpDrive(
@@ -145,106 +123,99 @@ public class testTeleOp extends OpMode {
             );
         }
 
-        if(intaking) {
-            intake.setPower(0.8);
-//            if(Math.abs(front.getDistance(DistanceUnit.MM) - frontDistance) > 50) {
-//                //TODO: ROTATE SPINDEXER 120 DEG
-//            }
-        } else if (spindexing) {
-            intake.setPower(0.5);
+        // LIMELIGHT TURRET
+        result = limelight.getLatestResult();
+        if (result.isValid()) {
+            for (LLResultTypes.FiducialResult fiducial : result.getFiducialResults()) {
+                pid = pidController.calculate(
+                        turret.getCurrentPosition(),
+                        turret.getCurrentPosition()
+                                - (537.7 * fiducial.getTargetYDegrees()) / 360.0
+                );
+                turret.setPower(pid);
+            }
         } else {
-            intake.setPower(0.0);
+            turret.setPower(0);
         }
 
-        if(gamepad1.a && !AisPressed) {
-            AisPressed = true;
+        double dist = front.getDistance(DistanceUnit.MM);
+        boolean ballDetectedNow = dist < BALL_DIST_MM;
+
+        // INTAKE STATE MACHINE
+        switch (intakeState) {
+
+            case IDLE:
+                intake.setPower(0);
+                if (ballsLoaded < 3) {
+                    intakeState = IntakeState.INTAKING;
+                }
+                break;
+
+            case INTAKING:
+                intake.setPower(0.8);
+
+                if (ballDetectedNow && !ballDetectedLast) {
+                    ballsLoaded++;
+                    rotateSpindexer120();
+                    intakeState = IntakeState.INDEXING;
+                    time = runtime.milliseconds();
+                }
+                break;
+
+            case INDEXING:
+                intake.setPower(0.5);
+
+                if (runtime.milliseconds() - time > 200) {
+                    intakeState = ballsLoaded < 3 ? IntakeState.INTAKING : IntakeState.IDLE;
+                }
+                break;
         }
-        if(!gamepad1.a && AisPressed) {
+
+        ballDetectedLast = ballDetectedNow;
+
+        // SHOOT BUTTON
+        if (gamepad1.a && !AisPressed) AisPressed = true;
+        if (!gamepad1.a && AisPressed) {
             AisPressed = false;
-            shoot = true;
+            if (ballsLoaded > 0 && shootState == ShootState.IDLE) {
+                shootRequested = true;
+            }
         }
 
-        // SHOOT SEQUENCE
-        if(shoot) {
-            leftShooter.setVelocity(-2250);
-            rightShooter.setVelocity(-2250);
-            time = runtime.milliseconds();
-            kickerUp1 = true;
-            shoot = false;
-            spindexing = true;
-        }
-        // FIRST SHOT
-        if(kickerUp1 && runtime.milliseconds() - time > 1000) {
-            leftShooter.setVelocity(-2250);
-            rightShooter.setVelocity(-2250);
-            kicker.setPosition(0.4);
-            kickerUp1 = false;
-            time = runtime.milliseconds();
-            kickerDown1 = true;
-        }
-        if(kickerDown1 && runtime.milliseconds() - time > 100) {
-            leftShooter.setVelocity(-2250);
-            rightShooter.setVelocity(-2250);
-            kicker.setPosition(0.225);
-            kickerDown1 = false;
-            time = runtime.milliseconds();
-            rotateSpin120_1 = true;
-        }
-        if(rotateSpin120_1 && runtime.milliseconds() - time > 10) {
-            leftShooter.setVelocity(-2250);
-            rightShooter.setVelocity(-2250);
-            //TODO: ROTATE SPINDEXER 120 DEG
-            spin1.setPosition(spin1.getPosition() + 0.169);
-            spin2.setPosition(spin2.getPosition() + 0.169);
-            rotateSpin120_1 = false;
-            time = runtime.milliseconds();
-            kickerUp2 = true;
-        }
+        // SHOOT STATE MACHINE
+        switch (shootState) {
 
-//        // SECOND SHOT
-//        if(kickerUp2 && runtime.milliseconds() - time > 3500) {
-//            leftShooter.setVelocity(-2250);
-//            rightShooter.setVelocity(-2250);
-//            kicker.setPosition(0.4);
-//            kickerUp2 = false;
-//            time = runtime.milliseconds();
-//            kickerDown2 = true;
-//        }
-//        if(kickerDown2 && runtime.milliseconds() - time > 1000) {
-//            leftShooter.setVelocity(-2250);
-//            rightShooter.setVelocity(-2250);
-//            kicker.setPosition(0.225);
-//            kickerDown2 = false;
-//            time = runtime.milliseconds();
-//            rotateSpin120_2 = true;
-//        }
-//        if(rotateSpin120_2 && runtime.milliseconds() - time > 2000) {
-//            leftShooter.setVelocity(-2250);
-//            rightShooter.setVelocity(-2250);
-//            //TODO: ROTATE SPINDEXER 120 DEG
-//            rotateSpin120_2 = false;
-//            time = runtime.milliseconds();
-//            kickerUp3 = true;
-//        }
-//
-//        // THIRD SHOT
-//        if(kickerUp3 && runtime.milliseconds() - time > 3500) {
-//            leftShooter.setVelocity(-2250);
-//            rightShooter.setVelocity(-2250);
-//            kicker.setPosition(0.4);
-//            kickerUp3 = false;
-//            time = runtime.milliseconds();
-//            kickerDown3 = true;
-//        }
-//        if(kickerDown3 && runtime.milliseconds() - time > 1000) {
-//            leftShooter.setVelocity(0);
-//            rightShooter.setVelocity(0);
-//            kicker.setPosition(0.225);
-//            kickerDown3 = false;
-//        }
+            case IDLE:
+                if (shootRequested) {
+                    leftShooter.setVelocity(-2250);
+                    rightShooter.setVelocity(-2250);
+                    kicker.setPosition(0.4);
+                    time = runtime.milliseconds();
+                    shootState = ShootState.KICK_UP;
+                    shootRequested = false;
+                }
+                break;
 
+            case KICK_UP:
+                if (runtime.milliseconds() - time > 120) {
+                    kicker.setPosition(0.225);
+                    time = runtime.milliseconds();
+                    shootState = ShootState.KICK_DOWN;
+                }
+                break;
 
-//        frontDistance = front.getDistance(DistanceUnit.MM);
-//        telemetry.update();
+            case KICK_DOWN:
+                if (runtime.milliseconds() - time > 100) {
+                    rotateSpindexer120();
+                    ballsLoaded--;
+                    shootState = ShootState.IDLE;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void stop() {
+        limelight.stop();
     }
 }
